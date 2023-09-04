06ef465008bc4fc266807d33460780647ec1721a
Viktorya <vika.kokorina.933@gmail.com>
2023-09-04 13:50:48 +0300

    Task_4

--- Task_4/Program.cs
+++ Task_4/Program.cs
@@ -6,10 +6,10 @@
 
 
 
-int max = 0;
+ int max = 0;
 
 Console.Write("Введите первое число: "); // ввели 1 данные
-int firstNumber = Convert.ToInt32(Console.ReadLine()); 
+int firstNumber = Convert.ToInt32(Console.ReadLine());
 /* присвоили переменную и так 3 раза, потому что у нас 3 числа и преобразовали строковое представление числа с указанным основанием системы счисления в эквивалентное 
 ему 32-битовое целое число со знаком, далее происходит чтение строки с переходом на новую строку
 */
@@ -22,22 +22,22 @@ int secondNumber = Convert.ToInt32(Console.ReadLine());
 Console.Write("Введите третье число: ");
 int threeNumber = Convert.ToInt32(Console.ReadLine());
 
-if(firstNumber > max) /* задали условие через if, и сравниваем сначала первое  число, больше ли оно max*/
+if (firstNumber > max) /* задали условие через if, и сравниваем сначала первое  число, больше ли оно max*/
 {
     max = firstNumber;
-  
+
 }
 
-if(secondNumber > max)
+if (secondNumber > max)
 {
     max = secondNumber;
-  
+
 }
 
-if( threeNumber > max)
+if (threeNumber > max)
 {
     max = threeNumber;
-  
+
 }
 
 Console.WriteLine("max = " + max);  /* вывели данные*/
